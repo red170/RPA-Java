901f22ce9382f16e5bd4d3ccf7705d5604ef6347
Ernesto Portillo <ernestoportillo170@gmail.com>
2024-12-03 15:38:57 -0600

    Actualizacion de Contenido

--- repo/src/Main.java
+++ repo/src/Main.java
@@ -12,47 +12,69 @@ public class Main {
     public static void main2(String[] args){
 
         //Numeros Enteros
-        byte numeroPequeno = 18; //Tamaño de 8 Bits (numeros desde -128 hasta 128)
-        short numeroMediano = 100; //Tamaño de 16 Bits (numeros desde -32767 hasta 32767)
-        int numeroGrande = 1000; //Tamaño de 32 Bits (numeros desde -2147483647 hasta 2147483647)
-        long numeroMuyGrande = 10000; //Tamaño de 64 Bits (numeros desde -9223372036854775807 hasta 9223372036854775807
-        System.out.println(numeroGrande);
+            byte numeroPequeno = 18; //Tamaño de 8 Bits (numeros desde -128 hasta 128)
+            short numeroMediano = 100; //Tamaño de 16 Bits (numeros desde -32767 hasta 32767)
+            int numeroGrande = 1000; //Tamaño de 32 Bits (numeros desde -2147483647 hasta 2147483647)
+            long numeroMuyGrande = 10000; //Tamaño de 64 Bits (numeros desde -9223372036854775807 hasta 9223372036854775807
+            System.out.println(numeroGrande);
 
         //Numeros Decimales
-        float decimal = 17.5f; //Siempre se Coloca la F al final del numero
-        double decimal1 = 19.5;
-        System.out.println(decimal);
+            float decimal = 17.5f; //Siempre se Coloca la F al final del numero
+            double decimal1 = 19.5;
+            System.out.println(decimal);
 
         //Boolenaos
-        boolean verdad = true;
-        boolean falso = false;
-        System.out.println(verdad);
+            boolean verdad = true;
+            boolean falso = false;
+            System.out.println(verdad);
 
         //Caracter
-        char caracter = 'a'; //Siempre se usan comillas simples en Char
-        System.out.println(caracter);
+            char caracter = 'a'; //Siempre se usan comillas simples en Char
+            System.out.println(caracter);
     }
 
     //STRING
     public static void main(String[] args){
-        String texto = "Hola Mundo";
+        //Almacenar Texto
+             String texto = "Hola Mundo";
 
-        int longitud = texto.length();
-        System.out.println(longitud);
+        //Metodos de String
+            //Imprimir longitud de String
+            int longitud = texto.length();
+            System.out.println(longitud);
+            //Reemplezar Texto
+            String texto2 = texto.replace("Hola", "Chao");
+            System.out.println(texto2);
+            //Verificar si un String Termina de cierta forma
+            System.out.println(texto.endsWith("undo"));
+            //Verificar si un String Comienza de cierta forma
+            System.out.println(texto.startsWith("H"));
+            //Verificar si un String Contiene ciertos caracteres o cadena de texto
+            System.out.println(texto.contains(" "));
+            //Verificar la Posicion de un caracter dentro de el String padre
+            System.out.println(texto.indexOf("o"));
+            //Convertir la cadena a Minusculas
+            System.out.println(texto.toLowerCase());
+            //Convertir la cadena a Mayusculas
+            System.out.println(texto.toUpperCase());
+            //Eliminar los espacios al Inicio y al Final del String
+            String malo = "    chancho feliz     ";
+            System.out.println(malo.trim());
 
-        String texto2 = texto.replace("Hola", "Chao");
-        System.out.println(texto2);
+       //Secuencias de Escape
+            //Al hacer uso del backslash "\" previo a un caracter este lo oculta de la secuencia de programacion y lo muestra como que fuese otro string mas
 
-        System.out.println(texto.endsWith("undo"));
-        System.out.println(texto.startsWith("H"));
-        System.out.println(texto.contains(" "));
-        System.out.println(texto.indexOf("o"));
+            String texto4 = "Hola \"Mundo\""; //Escapar Comillas
+            System.out.println(texto4);
 
-        System.out.println(texto.toLowerCase());
-        System.out.println(texto.toUpperCase());
+            String texto5 = "C:\\Hola \"Mundo\">";//Escapar Backslash
+            System.out.println(texto5);
 
+            String texto6 = "Hola \nMundo";//Insertar Salto de Linea dentro del String
+            System.out.println(texto6);
 
-        String malo = "    chancho feliz     ";
-        System.out.println(malo.trim());
+            String texto7 = "Hola \tMundo";//Insertar Tabulacion de Texto (Sangria)
+            System.out.println(texto7);
+
     }
 }
