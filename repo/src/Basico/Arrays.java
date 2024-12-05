b2455d7c5cb3efe9f21c2bc26f4867e31145038d
Ernesto Portillo <ernestoportillo170@gmail.com>
2024-12-05 13:27:29 -0600

    Reestructuracion del Contenido

---
+++ repo/src/Basico/Arrays.java
@@ -0,0 +1,16 @@
+package Basico;
+
+public class Arrays {
+    //ARRAYS
+    public static void main(String[] args){
+        int[] numeros = new int[5];
+        numeros[0] = 5;
+        numeros[1] = 50;
+        numeros[2] = 500;
+        numeros[3] = 5000;
+        numeros[4] = 50000;
+        System.out.println(java.util.Arrays.toString(numeros));
+
+        System.out.println(numeros.length);
+    }
+}
---
+++ repo/src/Basico/Variables.java
@@ -0,0 +1,28 @@
+package Basico;
+
+public class Variables {
+    //VARIABLES PRIMITIVAS
+    public static void main2(String[] args){
+
+        //Numeros Enteros
+        byte numeroPequeno = 18; //Tamaño de 8 Bits (numeros desde -128 hasta 128)
+        short numeroMediano = 100; //Tamaño de 16 Bits (numeros desde -32767 hasta 32767)
+        int numeroGrande = 1000; //Tamaño de 32 Bits (numeros desde -2147483647 hasta 2147483647)
+        long numeroMuyGrande = 10000; //Tamaño de 64 Bits (numeros desde -9223372036854775807 hasta 9223372036854775807
+        System.out.println(numeroGrande);
+
+        //Numeros Decimales
+        float decimal = 17.5f; //Siempre se Coloca la F al final del numero
+        double decimal1 = 19.5;
+        System.out.println(decimal);
+
+        //Boolenaos
+        boolean verdad = true;
+        boolean falso = false;
+        System.out.println(verdad);
+
+        //Caracter
+        char caracter = 'a'; //Siempre se usan comillas simples en Char
+        System.out.println(caracter);
+    }
+}
