d8086b70a03c4e3531e0de1042476ceca16cd8cc
Ernesto Portillo <ernestoportillo170@gmail.com>
2024-12-05 13:45:53 -0600

    Carga de Contenidos

---
+++ src/Basico/Arrays.java
@@ -0,0 +1,17 @@
+package Basico;
+
+
+public class Arrays {
+    //ARRAYS
+    public static void main1(String[] args){
+        int[] numeros = new int[5];
+        numeros[0] = 5;
+        numeros[1] = 50;
+        numeros[2] = 500;
+        numeros[3] = 5000;
+        numeros[4] = 50000;
+        System.out.println(java.util.Arrays.toString(numeros));
+
+        System.out.println(numeros.length);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//JAVA es un lenguaje orientado a Objetos por lo tanto la progrmamcion se basa en programar clases y objeto y dentro del archivo empezamos la programacion con una clase publica sobre la cual es la que iremos escribiendo el codigo
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    //HOLA MUNDO
+    public static void main(String[] args) {
+        System.out.println("Hola Ernesto");
+    }
+
+}
